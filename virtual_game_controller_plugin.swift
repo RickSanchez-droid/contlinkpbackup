255beb7cc5869752cde6d60cc40833e185000d96
RickSanchez-droid <deahansteyn@gmail.com>
2024-12-18 15:43:58 +0200

    hope this will resolve issue related to bluetooth updated vcontroller

--- virtual_game_controller_plugin.swift
+++ virtual_game_controller_plugin.swift
@@ -1,4 +1,3 @@
-swift
 import Flutter
 import GameController
 
@@ -15,18 +14,16 @@ class VirtualGameControllerPlugin: NSObject, FlutterPlugin {
 
     func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
-        case "registerVirtualController":
-            registerVirtualController(result: result)
-        case "sendButtonEvent":
-            sendButtonEvent(call: call, result: result)
-        case "sendAxisEvent":
-            sendAxisEvent(call: call, result: result)
+        case "registerController":
+            registerController(result: result)
+        case "sendInputEvent":
+            sendInputEvent(call: call, result: result)
         default:
             result(FlutterMethodNotImplemented)
         }
     }
 
-    private func registerVirtualController(result: @escaping FlutterResult) {
+    private func registerController(result: @escaping FlutterResult) {
         virtualController = GCController.controllers().first
         if virtualController == nil {
             virtualController = GCController()
@@ -36,38 +33,49 @@ class VirtualGameControllerPlugin: NSObject, FlutterPlugin {
     }
 
 
-    private func sendButtonEvent(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    private func sendInputEvent(call: FlutterMethodCall, result: @escaping FlutterResult) {
         guard let arguments = call.arguments as? [String: Any],
-              let buttonName = arguments["button"] as? String,
-              let pressed = arguments["pressed"] as? Bool else {
-            result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for sendButtonEvent", details: nil))
+              let type = arguments["type"] as? String,
+              let value = arguments["value"] as? Double else {
+            result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for sendInputEvent", details: nil))
             return
         }
-
-        // Simulate button press/release
-        if let virtualController = virtualController {
-            //  Replace with actual button mapping based on your controller setup.
-            if let button = getButton(for: buttonName, controller: virtualController) {
-                button.value = pressed ? 1 : 0
-            }
-        }
-        result(nil)
-    }
-
-    private func sendAxisEvent(call: FlutterMethodCall, result: @escaping FlutterResult) {
-        guard let arguments = call.arguments as? [String: Any],
-              let axisName = arguments["axis"] as? String,
-              let value = arguments["value"] as? Double else {
-                  result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for sendAxisEvent", details: nil))
-                  return
-              }
         
-        if let virtualController = virtualController {
-            // Replace with actual axis mapping
-            if let axis = getAxis(for: axisName, controller: virtualController) {
-                axis.value = CGFloat(value)
+        if type == "button" {
+            guard let buttonName = arguments["button"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for sendInputEvent", details: nil))
+                return
+            }
+            
+            if let virtualController = virtualController {
+                if let button = getButton(for: buttonName, controller: virtualController) {
+                    button.value = value > 0 ? 1 : 0
+                }
+            }
+        } else if type == "trigger" {
+            guard let triggerName = arguments["trigger"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for sendInputEvent", details: nil))
+                return
+            }
+            
+            if let virtualController = virtualController {
+                if let trigger = getAxis(for: triggerName, controller: virtualController) {
+                    trigger.value = CGFloat(value)
+                }
+            }
+        } else if type == "dpad" {
+            guard let direction = arguments["direction"] as? String else {
+                result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments for sendInputEvent", details: nil))
+                return
+            }
+            
+            if let virtualController = virtualController {
+                if let dpad = getDPad(for: direction, controller: virtualController) {
+                    dpad.value = value > 0 ? 1 : 0
+                }
             }
         }
+        
         result(nil)
     }
 
@@ -75,18 +83,30 @@ class VirtualGameControllerPlugin: NSObject, FlutterPlugin {
     // Helper functions to map button names to actual GCController elements
     private func getButton(for name: String, controller: GCController) -> GCControllerButtonInput? {
         switch name {
-        case "A": return controller.extendedGamepad?.buttonA
-        case "B": return controller.extendedGamepad?.buttonB
-        // Add more button mappings
+        case "buttonA": return controller.extendedGamepad?.buttonA
+        case "buttonB": return controller.extendedGamepad?.buttonB
+        case "buttonX": return controller.extendedGamepad?.buttonX
+        case "buttonY": return controller.extendedGamepad?.buttonY
+        case "leftShoulder": return controller.extendedGamepad?.leftShoulder
+        case "rightShoulder": return controller.extendedGamepad?.rightShoulder
         default: return nil
         }
     }
     
     private func getAxis(for name: String, controller: GCController) -> GCControllerAxisInput? {
         switch name {
-        case "leftStickX": return controller.extendedGamepad?.leftThumbstick?.xAxis
-        case "leftStickY": return controller.extendedGamepad?.leftThumbstick?.yAxis
-            // Add more axis mappings here
+        case "leftTrigger": return controller.extendedGamepad?.leftTrigger
+        case "rightTrigger": return controller.extendedGamepad?.rightTrigger
+        default: return nil
+        }
+    }
+    
+    private func getDPad(for name: String, controller: GCController) -> GCControllerDirectionPad? {
+        switch name {
+        case "up": return controller.extendedGamepad?.dpad.up
+        case "down": return controller.extendedGamepad?.dpad.down
+        case "left": return controller.extendedGamepad?.dpad.left
+        case "right": return controller.extendedGamepad?.dpad.right
         default: return nil
         }
     }
